2cd281f190100f4bf15481a8edc2ac569a868777
Yerassyl <tleugazy98@gmail.com>
2021-04-02 18:23:44 +0600

    Sdssdd

--- main.go
+++ main.go
@@ -1,91 +1,102 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	yt "github.com/kkdai/youtube"
+	"github.com/kkdai/youtube"
 	"github.com/urfave/cli"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"sync"
 )
 
 var (
-	directory string
+	path string
+	url  string
+	file string
 )
 
-func downloadFileFromUrl(url string) error {
-	currentDir, err := os.Getwd()
+func DownloadVideo(videoUrl string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	yt := youtube.NewYoutube(false, true)
+	err := yt.DecodeURL(videoUrl)
 	if err != nil {
-		return err
-	}
-	currentDir = currentDir + "/videos"
-	if directory != "" {
-		currentDir = directory
-	}
-	y := yt.NewYoutube(false, true)
-	if err := y.DecodeURL(url); err != nil {
 		log.Fatal(err)
 	}
-	fileName := fmt.Sprintf("%s_%s.mp4", y.Title, y.Author)
-	if err := y.StartDownload(currentDir, fileName, "high", 0); err != nil {
+	fileName := fmt.Sprintf("%s_%s.mp4", yt.Title, yt.Author)
+	err = yt.StartDownload(path, fileName, "high", 0)
+	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func DownloadCommand(c *cli.Context) error {
+	if url != "" {
+		//err := DownloadVideo(url)
+		//if err != nil {
+		//	return err
+		//}
+	} else if file != "" {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		text := string(data)
+		urls := strings.Split(text, "\r\n")
+		var wg sync.WaitGroup
+		for _, v := range urls {
+			wg.Add(1)
+			go DownloadVideo(v, &wg)
+		}
+		wg.Wait()
+	} else {
+		return errors.New("we need flags")
 	}
 	return nil
 }
 
 func main() {
-	cli.VersionFlag = cli.BoolFlag{
-		Name:  "print-version, V",
-		Usage: "print only the version",
-	}
 	app := cli.NewApp()
-	app.Name = "Template"
-	app.Version = "0.0.1"
+	app.Name = "Dar app"
+	app.Usage = "App for downloading videos from youtube"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "upload_directory, u_dir",
-			Value:       "",
-			Usage:       "directory where upload files",
-			Destination: &directory,
+			Name:        "path,p",
+			Value:       "./videos",
+			Usage:       "it is used for path",
+			Destination: &path,
 		},
 	}
 	app.Commands = []cli.Command{
 		{
 			Name:    "download",
-			Aliases: []string{"d"},
-			Usage:   "download something by url",
+			Aliases: []string{"down"},
+			Usage:   "it is command for downloading videos",
+			Action:  DownloadCommand,
 			Flags: []cli.Flag{
-				cli.StringFlag{Name: "url, u"},
-				cli.StringFlag{Name: "file, f"},
-			},
-			Action: func(c *cli.Context) error {
-				url := c.String("url")
-				file := c.String("file")
-				if url != "" {
-					err := downloadFileFromUrl(url)
-					if err != nil {
-						return err
-					}
-				} else if file != "" {
-					data, err := ioutil.ReadFile(file)
-					if err != nil {
-						log.Fatal(err)
-					}
-					urls := strings.Split(string(data), "\n")
-					for i, v := range urls {
-						fmt.Println(i, v)
-						v = strings.TrimSpace(v)
-						err = downloadFileFromUrl(v)
-						if err != nil {
-							return err
-						}
-					}
-				}
-				return nil
-
+				cli.StringFlag{
+					Name:        "url,u",
+					Usage:       "it is used for url",
+					Destination: &url,
+				},
+				cli.StringFlag{
+					Name:        "file,f",
+					Usage:       "file contained urls",
+					Destination: &file,
+				},
 			},
 		},
+	}
+	app.Action = func(c *cli.Context) error {
+		a := c.Args().Get(0)
+		fmt.Println("First Argument", a)
+		b := c.Args().Get(1)
+		fmt.Println("Second Argument", b)
+		//path := c.String("path")
+		fmt.Println("Path flag", path)
+		return nil
 	}
 	err := app.Run(os.Args)
 	if err != nil {
